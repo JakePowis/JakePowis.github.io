0b62ae43d959b9edeec4835daf6ea95b8b748fe7
Jake Powis <58339450+JakePowis@users.noreply.github.com>
2019-12-09 14:01:44 +0000

    Image carousel added to the page
    Adding JS & CSS files for the image carousel - should be working (had a bug for a long time that was driven by position of the <script> too early in the HTML, meaning variables used in JS wernt yet calculated when called upon in the carousel) - all fixed now and should be working. Image files also changed to local so need to upload.

---
+++ imageCarousel.js
@@ -0,0 +1,68 @@
+const carouselSlide = document.querySelector(".carousel-slide") // the slide
+const carouselImages = document.querySelectorAll(".carousel-slide img"); // the images
+
+
+
+//Buttons
+const prevButton = document.querySelector("#prevBtn");
+const nextButton = document.querySelector("#nextBtn");
+
+
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth; // get width of image so we know how much to move them along
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"; // move the starting positon on by "size" pixels 
+
+
+
+
+//Button listeners
+
+
+//when next button is clicked, move slide forward
+nextButton.addEventListener('click',()=>{
+	if(counter >= carouselImages.length-1) return; // in case of counter not resetting
+	carouselSlide.style.transition = "transform 0.6s ease-in-out";
+	counter++; // add one to counter on click
+	carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"; // move slide to the left as above - NOT WORKING
+
+})
+
+//when previous button is clicked, move slide back
+prevButton.addEventListener('click',()=>{
+	if(counter <= 0) return; // in case of counter not restting
+	carouselSlide.style.transition = "transform 0.6s ease-in-out";
+	counter--; // add one to counter on click
+	carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"; // move slide to the left as above - NOT WORKING
+
+})
+
+
+//for when at at of slideshow, reset to start
+carouselSlide.addEventListener("transitionend",()=>{ // if last slide (at start), skip back to first
+	if (carouselImages[counter].id === "last-clone") {
+	carouselSlide.style.transition = "none";
+	counter = carouselImages.length - 2; //reset count
+	carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+	}
+
+})
+
+
+//for when at start of slideshow, skip to last
+carouselSlide.addEventListener("transitionend",()=>{ // if first slide (at end), skip back to last
+	if (carouselImages[counter].id === "first-clone") {
+	carouselSlide.style.transition = "none";
+	counter = carouselImages.length - counter; //reset count
+	carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+	}
+
+})
+
+
+
+
+
+
+
