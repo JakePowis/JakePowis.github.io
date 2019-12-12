832989cb0c1e893d8375246fe7c2ff36eac5d21d
Jake Powis <58339450+JakePowis@users.noreply.github.com>
2019-12-12 12:48:49 +0000

    Added "Change Theme" functionality
    Using what i learnt in the FAC workshop, i'm using remove/add class DOM manipulation to allow the user to select a second theme (i.e a second set of classes for the main page elements) from a "button" at the bottom of the page. Also fixed some link issues.

---
+++ changeTheme.js
@@ -0,0 +1,71 @@
+
+let count = 0;
+
+function changeClass(){ // swaps out starting class for new class on tagged elements (alternating based on odd/even count)
+
+if(count % 2 == 0){
+
+document.getElementById("background1").classList.remove('background1');
+document.getElementById("background1").classList.add('background1a');
+
+document.getElementById("background2").classList.remove('background2');
+document.getElementById("background2").classList.add('background2a');
+
+document.getElementById("background3").classList.remove('background3');
+document.getElementById("background3").classList.add('background3a');
+
+document.getElementById("background4").classList.remove('background4');
+document.getElementById("background4").classList.add('background4a');
+
+document.getElementById("header").classList.remove('headerWrapper');
+document.getElementById("header").classList.add('headerWrapper2');
+
+document.getElementById("changeTheme").classList.remove('changeTheme');
+document.getElementById("changeTheme").classList.add('changeTheme2');
+
+document.getElementById("footer").classList.remove('footer');
+document.getElementById("footer").classList.add('footer2');
+
+
+
+}
+
+else {
+
+document.getElementById("background1").classList.remove('background1a');
+document.getElementById("background1").classList.add('background1');
+
+document.getElementById("background2").classList.remove('background2a');
+document.getElementById("background2").classList.add('background2');
+
+document.getElementById("background3").classList.remove('background3a');
+document.getElementById("background3").classList.add('background3');
+
+document.getElementById("background4").classList.remove('background4a');
+document.getElementById("background4").classList.add('background4');
+
+document.getElementById("header").classList.remove('headerWrapper2');
+document.getElementById("header").classList.add('headerWrapper');
+
+document.getElementById("changeTheme").classList.remove('changeTheme2');
+document.getElementById("changeTheme").classList.add('changeTheme');
+
+document.getElementById("footer").classList.remove('footer2');
+document.getElementById("footer").classList.add('footer');
+
+
+//WAY TO CHANGE ClASS ELEMETS SUCH AS BACKGROUND COLOR OF TEXTBOXS?
+
+
+
+}//end else
+
+count++
+
+} //end func
+
+
+
+window.onload = function(){
+     document.getElementById("changeTheme").addEventListener( 'click', changeClass); // on clicking the change class element, change class function is run and classes switched.
+    }
